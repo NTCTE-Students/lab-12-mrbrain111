ff56c8c874574bf93294c60bfe266fff04e029e4
mrbrain111 <ysolcev.nikita@nttek.ru>
2025-04-02 21:56:37 +0500

    Update index.php

--- lab12/public/index.php
+++ lab12/public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../autoload.php';
 
+use App\Models\User;
 use App\Models\Post;
 
 ?>
@@ -37,14 +38,22 @@ use App\Models\Post;
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?php print($post['title']); ?></h5>
                         </a>
                         <p class="mb-3 font-normal text-gray-700"><?php print(nl2br($post['body'])); ?></p>
-                        <a href="/post.php?id=<?php print($post['id']); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        <a href="/post.php?id=<?php print($post['id']); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 mb-3">
                             Read more
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                             </svg>
                         </a>
+                       <?php
+                       $userID = ($post['userId']);
+                       $user = (new User()) -> searchById($userID);
+                       ?>
+                        <a href="/post.php?id=<?php print($post['id']); ?>">
+                            <h5 class="mt-2 text-xl  tracking-tight text-gray-900">Пост создал: <?php print($user ->getData()['username'])?></h5>
+                        </a>
                     </div>
-                <?php }
+                <?php 
+                }
             ?>
         </section>
     </main>
